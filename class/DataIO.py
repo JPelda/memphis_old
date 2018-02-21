325d7cfe5f4ef850a4ecd86c677d093da6cdb82e
JPelda <johannes.pelda@hawk-hhg.de>
2018-02-21 11:04:53 +0100

    new folders and class

---
+++ class/DataIO.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 21 10:40:53 2018
+
+@author: jpelda
+"""
+import pandas as pd
+
+class DataIO:
+    def __init__(self):
+        pass
+
+    def write_to_sqlServer(self, sql_query):
+        pass
+
+    def read_from_sqlServer(self, sql_query, engine):
+        # TODO call(sql_query), transform result into pandas.DataFrame()
+        
+        
+        return df
+
+
+
+if __name__ == "__main__":
+    print('main')
+
+else:
+    print('else')
---
+++ main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 21 10:36:21 2018
+
+@author: jpelda
+"""
+
+import os
+import sys
+sys.path.append(os.getcwd() + os.sep + 'class')
+
+from DataIO import DataIO
+
+test_DataIO = DataIO()
+
