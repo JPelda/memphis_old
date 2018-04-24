23755dcfbe0626a00e63cb921f126ef6a33e3852
adifischerson <34036196+adifischerson@users.noreply.github.com>
2018-04-24 22:29:13 +0200

    csv to sql upload via py
    läd iwie auch eine leerzeile mit in die DB. bisher manuell im nachhinein gelöscht

--- archiv/MySQL_census_csv_into_db.py
+++ archiv/MySQL_census_csv_into_db.py
@@ -13,12 +13,15 @@ import numpy as np
 import os
 import pymysql
 
-print()
+#print()
 
-cnx = pymysql.connect(host='localhost', user='root',
-                             password='wasteheat', db='test')
+cnx = pymysql.connect(host='localhost',
+                      user='root',
+                      password='wasteheat', 
+                      db='memphis')
+
 cursor = cnx.cursor()
-engine = create_engine('mysql+pymysql://root:wasteheat@localhost:3306/test', echo=False)
+engine = create_engine('mysql+pymysql://root:wasteheat@localhost:3306/memphis', echo=False)
 #TABLES['census_ger'] = (
 #        "CREATE TABLE `census_ger` ("
 #        "  `grid_ID_100m` varchar(20) NOT NULL,"
@@ -31,31 +34,9 @@ engine = create_engine('mysql+pymysql://root:wasteheat@localhost:3306/test', ech
 
 
 #CSV_FILE = eval(os.path.dirname(os.getcwd()) + os.sep + "csv_Bevoelkerung_100m_Gitter" + os.sep + "Zensus_Bevoelkerung_100m-Gitter.csv")
+print("pls wait about 20 min - then reload localhost")
+query = "LOAD DATA INFILE 'E:/datamemphis/data/Zensus_Bevoelkerung_100m-Gitter.csv' INTO TABLE census_ger FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\\\\'"
 
-query = "LOAD DATA INFILE 'Zensus_Bevoelkerung_100m-Gitter.csv' INTO TABLE census_ger FIELDS TERMINATED BY ';' ENCLOSED BY '\"' ESCAPED BY '\\\\'"
-#load_data = "LOAD DATA INFILE `Zensus_Bevoelkerung_100m-Gitter.csv` INTO TABLE census_ger"
-#df = pd.DataFrame(np.random.randint(low=0, high=10, size=(4, 4)),
-#                  columns=['grid_ID_100m', 'x_mp_100m', 'y_mp_100m', 'inhabitans'])
-#ID = 'ID'
-#x = 'x'
-#y = 'y'
-#val = 'inhabitants'
-#pd.DataFrame()
-#fname = 'Zensus_Bevoelkerung_100m-Gitter.csv'
-#data_header = pd.read_csv(fname, delimiter=';', header=0, nrows=0)
-#
-#data = pd.read_csv(fname, delimiter=';', names=[ID, x, y, val],
-#                   skiprows=1)
-#print(data)
-
-#chunksize = 1000000
-#i = 0
-#for i in range(0, len(data), chunksize):
-#    pd.io.sql.to_sql(data[i:i+chunksize], name='census_ger',
-#                     con=engine, index=False, index_label='ID',
-#                     if_exists='append')
-#    print('\rdata left {}\r'.format(len(data) - i), end='')
-#data.to_sql(name='census_ger', con=engine)
 
 cursor.execute(query)
 
